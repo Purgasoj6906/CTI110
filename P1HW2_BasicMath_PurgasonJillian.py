3f1ab9b9e0db686c3330484855766315cfa52ef2
Purgasoj6906 <70550793+Purgasoj6906@users.noreply.github.com>
2020-08-31 21:12:20 -0400

    Add files via upload
    Basic Math Program and Pseudocode - First Submission

---
+++ P1HW2_BasicMath_PurgasonJillian.py
@@ -0,0 +1,17 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>   # A brief description of the project
+    # 08/31/2020
+    # CTI-110 P1HW2 - Basic Math
+    # Jillian Purgason
+    #
+
+>>> 
+= RESTART: C:/Users/j-pur/Desktop/CTI-110-0901/P1HW2_BasicMath_PurgasonJillian.py
+Please Enter first number:2
+Please Enter second number:3
+First number entered: 2
+Second number entered: 3
+Sum of both numbers: 5
+Result of Multiplying the two numbers: 6
+>>> 
---
+++ P1HW2_BasicMath_PurgasonJillian2.py
@@ -0,0 +1,15 @@
+  # A brief description of the project
+    # 08/31/2020
+    # CTI-110 P1HW2 - Basic Math
+    # Jillian Purgason
+    #
+
+num1 = int(input('Please Enter first number:'))
+num2 = int(input('Please Enter second number:'))
+print('First number entered:', num1)
+print('Second number entered:', num2)
+print('Sum of both numbers:', num1 + num2)
+print('Result of Multiplying the two numbers:', num1 * num2)
+
+
+    
