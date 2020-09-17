7f511e93a9214cc7c4f5367a931333e3b4f81795
Purgasoj6906 <70550793+Purgasoj6906@users.noreply.github.com>
2020-09-17 18:23:17 -0400

    First Submission

---
+++ P2LAB1_DrivingCosts_PurgasonJillian.py
@@ -0,0 +1,15 @@
+miles_per_gallon = float(input())
+
+dollars_per_gallon = float(input())
+
+dollars_per_mile = dollars_per_gallon / miles_per_gallon
+
+your_value1 = 20 * dollars_per_mile
+
+your_value2 = 75 * dollars_per_mile
+
+your_value3 = 500 * dollars_per_mile
+
+print('{:.2f} {:.2f} {:.2f}'.format(your_value1, your_value2, your_value3))
+
+
