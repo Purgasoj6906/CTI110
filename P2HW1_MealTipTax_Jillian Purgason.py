53da4d1730c5988001771bc82d7d17b3a49183aa
Purgasoj6906 <70550793+Purgasoj6906@users.noreply.github.com>
2020-09-17 20:20:23 -0400

    Add files via upload

---
+++ P2HW1_MealTipTax_Jillian Purgason.py
@@ -0,0 +1,36 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+# Meal Tip and Tax Calculator Project
+# 09/17/2020
+# CTI-110 P2HW1 - Meal Tip Tax Calculator
+# Jillian Purgason
+#
+>>> 
+= RESTART: C:/Users/j-pur/Desktop/CTI-110-0901/P2HW1_MealTIPTAX_JillianPurgason.py
+Enter Food Cost:100
+
+Enter Tip Percentage:15
+Enter Tax Percentage:6
+
+Calculated Tip: 15.0
+Calculated Tax: 6.0
+
+Total cost including tip and tax: 121.0
+>>> 
+#food_cost = float(input('Enter Food Cost:'))
+#print()
+#tip_percentage = float(input('Enter Tip Percentage:'))
+#tax_percentage = float(input('Enter Tax Percentage:'))
+#print()
+#
+#calculated_tip = float((food_cost * tip_percentage) / 100)
+#calculated_tax = float((food_cost * tax_percentage) / 100)
+#
+#total_cost = float(food_cost + calculated_tip + calculated_tax)
+#
+#print('Calculated Tip:', calculated_tip)
+#print('Calculated Tax:', calculated_tax)
+#print()
+#print('Total cost including tip and tax:', total_cost)
