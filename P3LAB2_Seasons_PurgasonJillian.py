60628f29cfe81f1443f81ff5903a25178342a595
Purgasoj6906 <70550793+Purgasoj6906@users.noreply.github.com>
2020-09-27 12:17:41 -0400

    Add files via upload

---
+++ P3LAB2_Seasons_PurgasonJillian.py
@@ -0,0 +1,41 @@
+input_month = input()
+input_day = int(input())
+
+
+if (input_month == 'March') and ((input_day > 19) and (input_day <= 31)):
+    print('Spring')
+    
+elif (input_month == 'April') and ((input_day >= 1) and (input_day <= 30)):
+    print ('Spring')
+elif (input_month == 'May') and ((input_day >= 1) and (input_day <= 31)):
+    print ('Spring')
+elif (input_month == 'June') and ((input_day >= 1) and (input_day <= 20)):
+    print ('Spring')
+elif (input_month == 'June') and ((input_day >= 21) and (input_day <= 30)):
+    print ('Summer')
+elif (input_month == 'July') and ((input_day >= 1) and (input_day <= 31)):
+    print ('Summer')
+elif (input_month == 'August') and ((input_day >= 1) and (input_day <= 31)):
+    print ('Summer')
+elif (input_month == 'September') and ((input_day >= 1) and (input_day <= 21)):
+    print('Summer')
+elif (input_month == 'September') and ((input_day >= 22) and (input_day <= 30)):
+    print('Autumn')
+elif (input_month == 'October') and ((input_day >= 1) and (input_day <= 31)):
+    print('Autumn')
+elif (input_month == 'November') and ((input_day >= 1) and (input_day <= 30)):
+    print('Autumn')
+elif (input_month == 'December') and ((input_day >= 1) and (input_day <= 20)):
+    print('Autumn')
+elif (input_month == 'December') and ((input_day >= 21) and (input_day <= 31)):
+    print('Winter')
+elif (input_month == 'January') and ((input_day >= 1) and (input_day <= 31)):
+    print('Winter')
+elif (input_month == 'February') and ((input_day >= 1) and (input_day <= 29)):
+    print('Winter')
+elif (input_month == 'March') and ((input_day >= 1) and (input_day <= 19)):
+    print('Winter')
+
+else:
+    print('Invalid')
+    
